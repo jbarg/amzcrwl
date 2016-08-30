6fdbf730c69f1193ecf3b3dfbc097459564be213
Jon <github@alleskaputt.com>
2016-08-30 19:31:36 +0200

    added defs

--- amzcrwl.py
+++ amzcrwl.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python2
+import requests
 
-
-# Buy Phase
-# 	-> Login
-# 	-> suche
-#	-> produkt aufrufen
-#	-> warenkorb
-
-
-# Kick aus warenkorb
-#	-> Login
-#	-> warenkorb aufrufen
-#	-> ware entfernen
 
 
 
@@ -31,7 +20,7 @@ def login(user, password):
     return sessionID
 
 
-def into_cart(url, sessionID):
+def add_to_cart(url, sessionID):
 
 	return 
 
@@ -45,6 +34,38 @@ def search(query_list, sessionID):
     return html_dom
 
 
+def req_product_page(url, sessionID):
+
+	html_dom = ''
+
+	return html_dom
+
+def get_cart_page(sessionID):
+
+
+	html_dom = ''
+
+	return html_dom
+
+def delete_from_cart(prudctID, sessionID):
+
+
+	foo = ''
+	return foo
 
 
 
+def main():
+
+
+	# Buy Phase
+	# 	-> Login
+	# 	-> suche
+	#	-> produkt aufrufen
+	#	-> warenkorb
+
+
+	# Kick aus warenkorb
+	#	-> Login
+	#	-> warenkorb aufrufen
+	#	-> ware entfernen
