14ef39b2a7c3e0d75f7707f3b6b875a3c19b400a
Jon <github@alleskaputt.com>
2016-08-30 19:14:54 +0200

    first commit

---
+++ amzcrwl.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2
+
+user = 'user'
+password = 'password'
+
+
+
+
+
+
+def login(user, password):
+
+    sessionID = ''
+
+    return sessionID
+
+
+def into_cart(url, sessionID):
+
+	return 
+
+
+
+def search(query_list, sessionID):
+
+    html_dom = ''
+
+
+    return html_dom
